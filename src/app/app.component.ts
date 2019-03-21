ade3a00a1115532ddd3179068db741059cea1636
shri <shree.3176@gmail.com>
2019-03-21 10:37:37 +0530

    Checked its working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { HttpErrorResponse } from "@angular/common/http";
+import { map } from 'rxjs/operators';
+import { WeatherService } from './weather.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.less']
 })
+
 export class AppComponent {
-  title = 'hichkiAngular4';
+
+  title = 'Shri angular 4 app';
+  chart = [];
+  other = [];
+  constructor(private _weather: WeatherService) { }
+  ngOnInit() {
+    this._weather.dailyForecast()
+      .subscribe(res => {
+
+        let wd: any = res;
+        let year_dataset = [];
+        let month_dataset = [];
+        let value_dataset = [];
+
+        for (let i = 0; i < wd.length; i++) {
+          //value12 = res[i].year;
+          year_dataset.push(res[i].year);
+          month_dataset.push(res[i].month);
+          value_dataset.push(res[i].value);
+        };
+
+        this.chart = new Chart('canvas', {
+          type: 'bar',
+          data: {
+            labels: year_dataset,
+            datasets: [
+              {
+                data: value_dataset,
+                borderColor: "#3cba9f",
+                fill: true
+              }
+              ,
+              {
+                data: month_dataset,
+                borderColor: "#ffcc00",
+                fill: true
+              },
+            ]
+          },
+          options: {
+            legend: {
+              display: true
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }],
+            }
+          }
+        });
+
+      })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { HttpClientModule } from "@angular/common/http";
+import { WeatherService } from './weather.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NewCmpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
