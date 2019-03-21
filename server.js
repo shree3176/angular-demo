a320973a75178d4af4a58ef887ea5fd5b135aa6c
shri <shree.3176@gmail.com>
2019-03-21 12:14:23 +0530

    8th Heroku commit

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname));
+process.env.PWD = process.cwd();
+
+app.use(express.static(process.env.PWD + '/build'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(___dirname, 'build', 'index.html'));
+    res.sendFile(path.join(process.env.PWD, '/build/index.html'));
 })
 console.log('Console listening');
