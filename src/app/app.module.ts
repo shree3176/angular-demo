680a89e9ae8dfb90fd0a18cd75728e42d898fa7d
shri <shree.3176@gmail.com>
2019-03-21 16:36:15 +0530

    added first article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
-import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { HttpClientModule } from "@angular/common/http";
-import { WeatherService } from './weather.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductsComponent,
-    NewCmpComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
