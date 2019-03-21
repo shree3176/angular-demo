2577ea2e5d1f96829cc2ef1fd6c7baa51162c0fb
shri <shree.3176@gmail.com>
2019-03-21 12:25:35 +0530

    10th Heroku commit

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-process.env.PWD = process.cwd();
-
-app.use(express.static(process.env.PWD));
+app.use(express.static(__dirname + '/dist/hichki-angular4'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(process.env.PWD, '/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/hichki-angular4/index.html'));
 })
 console.log('Console listening');
