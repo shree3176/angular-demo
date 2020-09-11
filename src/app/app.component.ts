bc5adc255bde2a44e098bab46b31b9d377a789a5
shri <shree.3176@gmail.com>
2020-09-11 14:44:33 +0530

    Add components blog, home, contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { HttpErrorResponse } from "@angular/common/http";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,11 @@ import { HttpErrorResponse } from "@angular/common/http";
 
 export class AppComponent {
   title = 'Angular Basics';
+
+  constructor(private _router: Router){}
+
+  navigateToProductDetail(){
+    this._router.navigate(["/blogs"]);                      
+ }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
+import { BlogComponent } from './blog/blog.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
